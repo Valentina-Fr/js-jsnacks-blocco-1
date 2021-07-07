20b083da92591f5f886aa66a2b189dbef17bc504
Valentina-Fr <v_frasca@virgilio.it>
2021-07-07 16:00:02 +0200

    Create arrays

---
+++ js/snack3.js
@@ -0,0 +1,18 @@
+/*  
+SNACK 3: Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+*/
+
+//Creo una lista di nomi
+var list1 = ["Elisabeth", "Mary", "Zelda", "Daisy", "Emily"]
+
+//Creo una lista di cognomi
+var list2 = ["White", "Green", "Brown", "Smith", "Black"];
+
+//Creo lista Gatsby vuota
+var gatsbyList = []; 
+
+console.table(list1);
+console.table(list2);
+console.table(gatsbyList);
+
+//Generare falsi nomi
