bf26034ab8fa2bfe9d0f257e2deb0fd245f47a4b
Valentina-Fr <v_frasca@virgilio.it>
2021-07-07 16:30:03 +0200

    Add elements

--- js/snack4.js
+++ js/snack4.js
@@ -10,4 +10,15 @@ var list1 = [15, 25, 36, 44, 50];
 //Creare secondo array
 var list2 = [5, 10, 18, 22, 27, 29, 33, 41, 56, 60, 71, 88];
 
-//Aggiungere elementi casuali all'array con meno elemnenti
+//Aggiungere elementi casuali all'array con meno elementi
+while (list1.length !== list2.length) {
+    var random = Math.floor(Math.random() * 100);
+    if (list1.length < list2.length) {
+        list1.push(random);
+    } else {
+        list2.push(random);
+    }
+}
+
+console.table(list1);
+console.table(list2);
