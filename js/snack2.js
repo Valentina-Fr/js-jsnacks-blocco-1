f0a54c55b8b76ab6484592478d84946310617e0b
Valentina-Fr <v_frasca@virgilio.it>
2021-07-07 15:48:26 +0200

    Print result

--- js/snack2.js
+++ js/snack2.js
@@ -7,3 +7,10 @@ se è dispari stampa il numero successivo.
 do { 
 var userNumber = parseInt(prompt("Inserisci un numero"));
 } while (isNaN(userNumber) || userNumber <= 0);
+
+//Stabilire se è pari o dispari e stampare
+if (userNumber % 2 === 0) {
+    console.log(userNumber);
+} else {
+    console.log(userNumber + 1);
+}
