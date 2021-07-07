ee6c09fcdfdd89ba6f05b1e805fc4b198088f532
Valentina-Fr <v_frasca@virgilio.it>
2021-07-07 16:17:15 +0200

    Create 2 arrays

---
+++ js/snack4.js
@@ -0,0 +1,13 @@
+/*  
+SNACK 4: Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro.
+*/
+
+//Creare primo array
+var list1 = [15, 25, 36, 44, 50];
+
+//Creare secondo array
+var list2 = [5, 10, 18, 22, 27, 29, 33, 41, 56, 60, 71, 88];
+
+//Aggiungere elementi casuali all'array con meno elemnenti
