6a9f9391dee9371f0a875538df73508472e39837
Valentina-Fr <v_frasca@virgilio.it>
2021-07-07 15:37:35 +0200

    With while

--- js/snack1.js
+++ js/snack1.js
@@ -4,17 +4,20 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.
 */
 
-//Versione con for
-
 //Dichiarazione e assegnazion variabile sum 
 var sum = 0;
 
-//Ciclo for
-for (var i = 0; i < 5; i++) {
+//Versione con for
+/* for (var i = 0; i < 5; i++) {
     var userNumber = parseInt(prompt("Inserisci un numero", "1"));
     sum += userNumber;
-}
-
-console.log(sum);
+} */
 
 //Versione con while
+var i = 0;
+while (i < 5) {
+    var userNumber = parseInt(prompt("Inserisci un numero", "1"));
+    sum += userNumber;
+    i++;
+}
+console.log(sum);
